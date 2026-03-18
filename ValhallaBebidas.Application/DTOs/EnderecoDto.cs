4ff53936a025a35163382cfbeff8156978598ebf
RODRIGO LUIS SILVA BENTO <rodrigo.lsbento@senacsp.edu.br>
2026-03-17 22:27:25 -0300

    Dtos

--- ValhallaBebidas.Application/DTOs/EnderecoDto.cs
+++ ValhallaBebidas.Application/DTOs/EnderecoDto.cs
@@ -9,7 +9,7 @@
         public string Cep { get; set; }
         public string Bairro { get; set; }
         public string Cidade { get; set; }
-        public string Estado { get; set; }
+        public string Estado { get; set; } 
 
     }
 
