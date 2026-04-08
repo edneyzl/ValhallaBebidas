c0b03c9216d35a42f3f752543d9410eb0ca24c67
RODRIGO LUIS SILVA BENTO <rodrigo.lsbento@senacsp.edu.br>
2026-04-07 21:45:09 -0300

    UI

---
+++ ValhallaBebidas.UI/FrmLogin.cs
@@ -0,0 +1,15 @@
+namespace ValhallaBebidas.UI
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
